daa9b51d304fa6a394337a5e9d0880800010fe42
Kyelie Clark <kyelieclark@mail.weber.edu>
2025-11-11 09:43:02 -0700

    ADD: the dog barks

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from animal import Animal
+from dog import Dog
 
 if __name__ == "__main__":
 
     kitty = Animal("kitty", "Feline")
+    fido= Dog("Fido", "Caine", "Bulldog")
 
     print (kitty)
+    print (fido)
     kitty.speak()
+    fido. speak()
+
     # TODO: Print the Animal instance
     # TODO: Call the method to make a generic animal sound
 
